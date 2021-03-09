0d8f44895c1994c896ac4b0c663e666cd55d832d
fajri228 <mhammadfajri05@gmail.com>
2021-03-09 10:07:46 +0700

    praktikum1

---
+++ dart_application_1/bin/dasar_dart.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+import 'dart:io';
+
+main(List<String> arguments) {
+  int number = int.tryParse(stdin.readLineSync());
+
+  if (number > 0) {
+    print("positif");
+  } else if (number < 0) {
+    print("negatif");
+  } else {
+    print("nol");
+  }
+}
